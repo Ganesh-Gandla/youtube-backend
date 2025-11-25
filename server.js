d192796b7722cdd40832e5411fd70a63d5ec23e2
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-25 16:20:17 +0530

    Backend Initialization Completed

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Connect Database
+connectDB();
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("YouTube Clone Backend Running...");
+});
+
+const PORT = process.env.PORT || 5100;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
